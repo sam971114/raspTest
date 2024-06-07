2af44a2e61be2f0f927567ee2ec5b176d4b22913
sam971114 <simonana@naver.com>
2024-06-07 16:13:36 +0900

    feat: deviceId 추가

--- test.js
+++ test.js
@@ -5,12 +5,14 @@ const mqtt = require('mqtt');
 const caFile = fs.readFileSync('./AWS_Things_key/rootCA.pem');
 const certFile = fs.readFileSync('./AWS_Things_key/certificate.pem.crt');
 const keyFile = fs.readFileSync('./AWS_Things_key/private.pem.key');
+const deviceId = 'YOUR_DEVICE_ID_HERE';
 
 // MQTT options including the SSL configuration
 const options = {
   host: 'a3mwllq937a5i6-ats.iot.ap-northeast-2.amazonaws.com',
   port: 8883,
   protocol: 'mqtts',
+  clientId: deviceId,
   ca: caFile,
   cert: certFile,
   key: keyFile,
@@ -20,29 +22,16 @@ const options = {
 // Connect to the AWS IoT
 const client = mqtt.connect(options);
 
-client.on('connect', () => {
-  console.log('Connected to AWS IoT');
-  publishData();
-});
-
-client.on('error', (error) => {
-  console.log('Connection failed:', error);
-});
-
-function publishData() {
-  let ctr = 1;
-  setInterval(() => {
-    const msg = "Testing" + ctr;
-    console.log(msg);
-    client.publish('raspi/data', JSON.stringify({ msg: msg }), { qos: 0, retain: false });
-    ctr++;
-  }, 5000);
-}
-
-// This will keep the Node.js process running even if the publishing function is idle
-process.on('SIGINT', function() {
-  console.log("Caught interrupt signal");
-  client.end(true, () => {
-    process.exit();
+client.on('connect', function () {
+  console.log('Connected to AWS IoT Core');
+  
+  // 연결 성공 시, 특정 토픽에 메시지 발행
+  client.publish(topic, 'Hello from Raspberry Pi!', {}, function () {
+    console.log('Message sent to ' + topic);
+    client.end(); // 메시지 발행 후 연결 종료
   });
+});
+
+client.on('error', function (error) {
+  console.error('Connection failed:', error);
 });
